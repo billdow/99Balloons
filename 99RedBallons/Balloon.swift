be54aade0a04c40dc6ce33b626d1b9c7e91979c6
Bill <dowbill@hotmail.com>
2014-12-27 08:30:56 -0800

    second commit

--- 99RedBallons/Balloon.swift
+++ 99RedBallons/Balloon.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+import UIKit
+
+struct Balloon {
+    var number = 0
+    var image = UIImage(named: "")
+    
+    
+    
+    
+    
+}
--- 99RedBallons/ViewController.swift
+++ 99RedBallons/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballonslabel: UILabel!
+    
+    @IBOutlet weak var redBallonsImage: UIImageView!
+    
+    var balloons:[Balloon] = []
+    
+    var currentIndex = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        createBalloons()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +33,38 @@ class ViewController: UIViewController {
     }
 
 
+    
+    @IBAction func nextButtonPressedAction(sender: UIBarButtonItem) {
+
+    let balloon = balloons[currentIndex]
+    ballonslabel.text = "\(balloon.number) Balloon"
+    //backgroundImageView.image = balloon.image
+    currentIndex += 1
+
 }
 
+
+func createBalloons(){
+    for var balloonCount = 0; balloonCount <= 99; ++balloonCount
+    {
+        
+        var balloon = Balloon()
+        balloon.number = balloonCount
+        
+      let randonNumber = Int(arc4random_uniform(UInt32(UInt32(4))))
+       
+        switch randonNumber {
+            case 1:
+            balloon.image = UIImage(named: "RedBalloon1.jpg")
+            case 2:
+            balloon.image = UIImage(named: "Redballoon2.jpg")
+            case 3:
+            balloon.image = UIImage(named: "Redballoon3.jpg")
+        default:
+            balloon.image = UIImage(named: "Redballoon4.jpg")
+          }
+     self.balloons.append(balloon)
+        
+            }
+}
+}
